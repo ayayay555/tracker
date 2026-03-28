818492269cebecc1b1c2e901f282aee0428f2ae9
ayayay555 <karljhunzgonzales@gmail.com>
2026-03-28 03:39:32 -0700

    Rebrand: Renamed to Curl and optimized Mobile UX (disabled DevicePreview on mobile)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:device_preview/device_preview.dart';
 import 'models/bank.dart';
 import 'models/transaction.dart';
@@ -7,7 +8,7 @@ import 'logic/transaction_manager.dart';
 void main() {
   runApp(
     DevicePreview(
-      enabled: true,
+      enabled: !kIsWeb ? false : true, // Only enabled on Web for preview
       builder: (context) => const CurlApp(),
     ),
   );
